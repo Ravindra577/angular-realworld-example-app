c45d68cd33d0dde1026f6db4c2822be60933c5a1
Ravindra577 <ravindra.cse577@gmail.com>
2020-05-18 15:14:49 +0530

    Added no reboot

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #creating images
-aws ec2 create-image --instance-id "$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)" --name "AngularServer.$(date +"%Y%m%d_%H%M%S")" --description "$(date)"
+aws ec2 create-image --instance-id "$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)" --name "AngularServer.$(date +"%Y%m%d_%H%M%S")" --description "$(date)" --no-reboot
